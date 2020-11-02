4117758ce4a1999b8ff044f3b3358fd5c691118c
jeremiaaxel <jeremiaaxelbachtera@gmail.com>
2020-11-02 08:34:37 +0700

    Buat fungsi strIsEqual; unuse string.h

--- main/main.c
+++ main/main.c
@@ -1,11 +1,28 @@
 /* Include(s) */
 #include <stdio.h>
-#include <string.h>
+#include "./../header/boolean.h"
 
 void ShowMenu() {
     printf("new : New Game\n");
     printf("load : Load Game\n");
     printf("exit : Exit Game\n");
+}
+
+/* fungsi membandingkan dua string */
+// pindahin ke mana ya?
+boolean strIsEqual(char * s1, char * s2) {
+    int i;
+    boolean eq;
+
+    eq = true;
+    i = 0;
+    while (eq && (s1[i] != '\0') && (s2[i] != '\0')) {
+        if (s1[i] != s2[i]) {
+            eq = false;
+        }
+        i++;
+    }
+    return eq;
 }
 
 /* Main */
@@ -23,16 +40,16 @@ int main () {
     ShowMenu();
     fgets(main_opt, 5, stdin);
 
-    while (strcmp(main_opt, exit) != 0) {
-        if (strcmp(main_opt, new) == 0) {
+    while (!strIsEqual(main_opt, exit)) {
+        if (strIsEqual(main_opt, new)) {
             // ke new
             printf("new\n");
-        } else if (strcmp(main_opt, load) == 0) {
+        } else if (strIsEqual(main_opt, load)) {
             // ke load
             printf("load\n");
         }
         // Request input
-        menu();
+        ShowMenu();
         fgets(main_opt, 5, stdin);
     }
 
